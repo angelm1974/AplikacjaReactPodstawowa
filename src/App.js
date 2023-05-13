01f8029026354bb706e42418703f9a0cf3186eff
angelm1974 <mariusz980@gmail.com>
2023-05-13 11:28:41 +0200

    formularz

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import MojPrzycisk from './components/MojPrzycisk';
-
+// import MojPrzycisk from './components/MojPrzycisk';
+import Formularz from './components/Formularz';
 
 // const dodajJeden = () => {
 //   liczba = liczba + 1;
@@ -9,41 +9,13 @@ import MojPrzycisk from './components/MojPrzycisk';
 // };
 
 function App() {
-  let [liczba, setLiczba] = useState(0);
-  let [tekst, setText] = useState("liczba jest równa zero");
-
-  const dodajJeden = () => {
-    setLiczba(liczba + 1);
-    dynamicznyTekst();
-    console.log(liczba);
-  };
-
-  const dynamicznyTekst = () => {
-    let mojtekst = "";
-    if (liczba > 0 && liczba < 5) {
-      mojtekst = "liczba jest większa od zera";
-    } else if (liczba > 5) {
-      mojtekst = "liczba jest większa od 5";
-    }
-    setText(mojtekst);
-  };
-   
-  useEffect(() => {
-    dynamicznyTekst();
-  });
-
+ 
   return (
     <div className="App">
       
       <header className="App-header">
-      <h2>{tekst}</h2>
-        <div className="moj-text">liczba wynosi: {liczba}</div>
-        <button type="button"
-          onClick={dodajJeden}
-          className="btn btn-primary">Dodaj jeden</button>
-        <MojPrzycisk funkcja="zmienTekst" klasaPrzycisku="btn btn-primary" tekstPrzycisku="ok" />
-        <MojPrzycisk funkcja ="zapiszTekst" klasaPrzycisku="btn btn-warning" tekstPrzycisku="anuluj"/>
-      </header>
+       <Formularz />
+       </header>
     </div>
   );
 }
---
+++ src/components/Formularz.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+
+export default function Formularz(props) {
+  const [imie, setImie] = useState("");
+  const [nazwisko, setNazwisko] = useState("");
+  const [email, setEmail] = useState("");
+  const [haslo, setHaslo] = useState("");
+
+  const zapisz = (event) => {
+    event.preventDefault();
+    props.onSubmit({ imie, nazwisko, email, haslo });
+    setImie("");
+    setNazwisko("");
+    setEmail("");
+    setHaslo("");
+  };
+
+  return (
+    <div className="mt-4">
+      <div style={{ verticalAlign: "top" }}>Formularz</div>
+      <form onSubmit={zapisz}>
+        <div class="mb-3">
+            <label for="imie" class="form-label">
+                Podaj imię</label>
+            <input 
+                type="text"
+                class="form-control"
+                id="frmimie"
+                placeholder="Podaj imię"
+                value={imie}
+                onChange={(event) => setImie(event.target.value)}
+            />
+        </div>
+        <div class="mb-3">
+            <label for="nazwisko" class="form-label">
+                Podaj nazwisko</label>
+            <input 
+                type="text"
+                class="form-control"
+                id="frmnazwisko"
+                placeholder="Podaj nazwisko"
+                value={nazwisko}
+                onChange={(event) => setNazwisko(event.target.value)}
+            />
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">
+                Podaj nazwisko</label>
+            <input 
+                type="email"
+                class="form-control"
+                id="frmemail"
+                placeholder="Podaj emaila"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+            />
+        </div>
+        <div class="mb-3">
+            <label for="haslo" class="form-label">
+                Podaj hasło</label>
+            <input 
+                type="password"
+                class="form-control"
+                id="frmhaslo"
+                placeholder="Podaj haslo"
+                value={haslo}
+                onChange={(event) => setHaslo(event.target.value)}
+            />
+        </div>
+        <button type="submit" class="btn btn-primary">
+            Zapisz
+        </button>
+      </form>
+    </div>
+  );
+}
